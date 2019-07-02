af6ce29b607d0da8b39fb16fa6b9f327e1bc19de
G0d3r <ernest.godlewski@icloud.com>
2019-07-02 23:02:19 +0200

    Next commit

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,14 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentValue: Int = 50;
+    var targetValue: Int = 0;
+    
+    @IBOutlet weak var slider: UISlider!;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let roundedValue = slider.value.rounded();
+        currentValue = Int(roundedValue);
+        targetValue = Int.random(in: 1...100);
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is now: \(currentValue)";
+        let message = "The value of the slider is now: \(currentValue)\nThe target value is \(targetValue)";
         let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert);
         let action = UIAlertAction(title: "OK", style: .default, handler: nil);
         alert.addAction(action);
@@ -29,6 +35,11 @@ class ViewController: UIViewController {
         let roundedValue = slider.value.rounded();
         print("The value of the slider is now: \(roundedValue)");
         currentValue = Int(roundedValue);
+    }
+    
+    func startNewValue() {
+        targetValue = Int.random(in: 1...100);
+        
     }
 
 }
