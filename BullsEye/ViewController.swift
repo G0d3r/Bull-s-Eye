c5aeed1df6195fea50b5784c687f33c6ae3409b8
Ernest Godlewski <Ernest.Godlewski@tomtom.com>
2019-07-05 17:18:29 +0200

    Some changes

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var currentValue: Int = 50;
     var targetValue: Int = 0;
     
     @IBOutlet weak var slider: UISlider!;
+    @IBOutlet weak var label: UILabel!;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,26 +22,53 @@ class ViewController: UIViewController {
         let roundedValue = slider.value.rounded();
         currentValue = Int(roundedValue);
         targetValue = Int.random(in: 1...100);
+        startNewValue();
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is now: \(currentValue)\nThe target value is \(targetValue)";
+        //        var difference: Int;
+        //        if currentValue > targetValue {
+        //            difference = currentValue - targetValue;
+        //        } else if currentValue < targetValue{
+        //            difference = targetValue - currentValue;
+        //        } else {
+        //            difference = 0;
+        //        }
+        
+        //        if currentValue > targetValue {
+        //            difference = currentValue - targetValue;
+        //        } else if currentValue < targetValue{
+        //            difference = (currentValue - targetValue) / (-1);
+        //        } else {
+        //            difference = 0;
+        //        }
+        
+        var difference = currentValue - targetValue;
+        if difference < 0 {
+            difference = difference * (-1);
+        }
+        
+        let message = "The value of the slider is now: \(currentValue)\nThe target value is \(targetValue)\nThe difference is \(difference)";
         let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert);
         let action = UIAlertAction(title: "OK", style: .default, handler: nil);
         alert.addAction(action);
         present(alert, animated: true, completion: nil);
+        startNewValue();
     }
-
+    
     @IBAction func slider(_ slider: UISlider) {
         let roundedValue = slider.value.rounded();
         print("The value of the slider is now: \(roundedValue)");
         currentValue = Int(roundedValue);
+        label.text = String(currentValue);
     }
+    
     
     func startNewValue() {
         targetValue = Int.random(in: 1...100);
-        
+        currentValue = 50;
+        slider.value = Float(currentValue);
     }
-
+    
 }
 
